0a87c2699c34dfcfb6aa107cab9044332868742c
Martzehh <martzehh@gmail.com>
2021-02-11 13:41:49 -0500

    Adding Lunenburg Project

---
+++ lunenburg_lens/script.js
@@ -0,0 +1,18 @@
+const landingPage = document.getElementById("landing-page");
+
+const slideshow = () => {
+// Basic function to toggle landing page background images.
+    let counter = 0;
+    setInterval(function() {
+        if (counter === 0) {
+            landingPage.classList.toggle("banner-two");
+            counter++;
+        } else if(counter === 1) {
+            landingPage.classList.toggle("banner-three");
+            counter++;
+        } else {
+            counter = 0;
+        }
+    }, 1000);
+}
+slideshow();
